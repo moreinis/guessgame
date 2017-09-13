73f194f45363712836f31ed1bdbc506de6ae3732
moreinis <31930502+moreinis@users.noreply.github.com>
2017-09-13 18:03:13 -0400

    Functioning Input and Output
    But no guessing game! Needs integration.

--- guess.js
+++ guess.js
@@ -1,50 +1,20 @@
+testmessage();
+document.getElementsByName("userInput")[0].addEventListener('change', readGuess);
 
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function playGame(player) {
-    var Turns = 0;
-    var Correct = 0;
-	var State = 0;
-    var Answer = getRandomInt(1,10);
-    do {
-        var Guess = prompt("Guess the integer between 0 and 100!");
-        Turns++;
-        if (isNaN(Guess) !== true) {
-            if (Guess == Answer) {
-                alert("CORRECT! You guessed it in "+Turns+" turns.");
-                Correct = 1;
-            }
-            else if (Guess > Answer) {
-                alert("Hint: Try a lower number!");
-            }
-            else {
-                alert("Hint: Try a higher number!");
-            }
-        }
-        else {
-            alert("Wrong!");
-            player.losses ++;
-        }
-    }
-    while (Correct == 0);
-	while (State == 0);
+function readGuess(){
+   message(this.value);
+   document.getElementById("userInput").value= "";
 }
 
 function testmessage() {
-		var saythis="something to say";
+		var saythis="Guessing Game!";
 		message(saythis); // Call the function Message and pass it the value of the variable Saythis
-		x=0;
-		while (x<1000){
-			message("*");
-			x++;
-			}
-		var saythis="something else to say";
-		message(saythis);
+		var delayMillis = 1000; //1 second
+		setTimeout(function() {
+			var saythis="Enter a guess (1-100)";
+			message(saythis);
+		}, delayMillis);
+
 }
 
 function start() {
@@ -57,12 +27,10 @@ function start() {
 }
 
 function message (saythis){
-	var containdiv = document.getElementById("message");   // Get the <div> element with id="message"  
+	var containdiv = document.getElementById("message");   // Get the <div> element with id="message"
 	var para = document.createElement("p");
 	var node = document.createTextNode(saythis);
 	para.appendChild(node);
 	containdiv.removeChild(containdiv.childNodes[0]);
 	document.getElementById("message").appendChild(para);
 }
-alert("This is a guessing game, which you will prevail at!");
-testmessage();
