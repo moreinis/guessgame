5eaee976a1af255b70cd041435aceac710689808
moreinis <31930502+moreinis@users.noreply.github.com>
2017-09-18 21:15:07 -0400

    Fixed Randomizer

--- guess.js
+++ guess.js
@@ -1,13 +1,14 @@
 /* NOTE: This code all needs commenting! - MBM */
+
+// see https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
 function getRandomInt(min, max) {
-  // see https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function playGame(player) {
     var Turns = 0;
     var Answered = 0;
-    var Answer = getRandomInt(1,101);
+    var Answer = getRandomInt(1,100);
     do {
         var Guess = prompt("Guess the integer between 0 and 100!");
         Turns++;
